6fd8e79efa06f8e2a2887b32de559bb755cf1889
Keting Chen <kchen346@wisc.edu>
2023-04-12 07:00:31 +0800

    Multiple thread clock

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,9 @@
-use std::{collections::HashMap, process::id};
+use std::{
+    collections::HashMap,
+    process::id,
+    sync::{Arc, Mutex},
+};
+extern crate num_cpus;
 
 #[derive(Clone)]
 struct Entry<K: Clone> {
@@ -11,12 +16,64 @@ struct MapEntry<V> {
     index: usize,
 }
 
+pub struct ClockDashMap<K: Clone, V> {
+    num_shards: usize,
+    shards: Vec<Arc<Mutex<ClockMap<K, V>>>>,
+    key_shard: Arc<Mutex<HashMap<K, usize>>>,
+    next_modify: Arc<Mutex<usize>>,
+}
+
 pub struct ClockMap<K: Clone, V> {
     value_map: HashMap<K, MapEntry<V>>,
     clock_list: Vec<Option<Entry<K>>>,
     pointer: usize,
     miss: i32,
     capacity: usize,
+}
+
+impl<K: std::cmp::Eq + std::hash::Hash + Clone, V: Clone> ClockDashMap<K, V> {
+    fn new(cap: usize) -> Self {
+        let num_shards = num_cpus::get();
+        let mut shards = Vec::default();
+        for i in 0..num_shards {
+            let mut this_cap = (cap - cap % num_shards) / num_shards;
+            if i == num_shards - 1 {
+                this_cap += cap % num_shards;
+            }
+            shards.push(Arc::new(Mutex::new(ClockMap::new(this_cap))));
+        }
+        ClockDashMap {
+            num_shards: num_shards,
+            shards: shards,
+            key_shard: Arc::new(Mutex::new(HashMap::default())),
+            next_modify: Arc::new(Mutex::new(0)),
+        }
+    }
+    fn insert(&mut self, key: K, val: V) {
+        let key_shard = self.key_shard.lock().unwrap();
+        if (*key_shard).contains_key(&key) {
+            let shard_idx = *key_shard.get(&key).unwrap();
+            let shard = &mut self.shards[shard_idx].lock().unwrap();
+            (*shard).insert(key.clone(), val);
+        } else {
+            let shard_idx = self.next_modify.lock().unwrap();
+            let mut next_modify = self.next_modify.lock().unwrap();
+            *next_modify = (*next_modify + 1) % self.num_shards;
+            let mut key_shard = self.key_shard.lock().unwrap();
+            (*key_shard).insert(key.clone(), *shard_idx);
+            let shard = &mut self.shards[*shard_idx].lock().unwrap();
+            (*shard).insert(key, val);
+        }
+    }
+    fn read(&mut self, key: &K) -> Option<V> {
+        let key_shard = self.key_shard.lock().unwrap();
+        if (*key_shard).contains_key(key) {
+            let shard_idx = *key_shard.get(key).unwrap();
+            let shard = &mut self.shards[shard_idx].lock().unwrap();
+            return (*shard).read(key);
+        }
+        return None;
+    }
 }
 
 impl<'a, K: std::cmp::Eq + std::hash::Hash + Clone, V: Clone> ClockMap<K, V> {
@@ -30,7 +87,7 @@ impl<'a, K: std::cmp::Eq + std::hash::Hash + Clone, V: Clone> ClockMap<K, V> {
         }
     }
     fn read(&mut self, key: &K) -> Option<V> {
-        if self.value_map.contains_key(&key) {
+        if self.value_map.contains_key(key) {
             let idx = self.value_map.get(&key).unwrap().index;
             if let Some(Entry {
                 key: entry_key,
@@ -50,7 +107,7 @@ impl<'a, K: std::cmp::Eq + std::hash::Hash + Clone, V: Clone> ClockMap<K, V> {
             let idx = self.value_map.get(&key).unwrap().index;
             if self.clock_list[idx].as_ref().unwrap().key == key {
                 match &mut self.clock_list[idx] {
-                    None => {}
+                    None => {} // need to throw exceptions?
                     Some(Entry { second_chance, .. }) => {
                         *second_chance = true;
                     }
